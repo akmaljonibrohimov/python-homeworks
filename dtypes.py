c392877b3345647cfdef2e91fed0eb55b99bb5c6
theibroximov <157703303+akmaljonibrohimov@users.noreply.github.com>
2024-12-30 19:23:24 +0500

    Add files via upload

---
+++ dtypes.py
@@ -0,0 +1,22 @@
+#  Data Type - Ma'lumot toifasi
+
+# Primative data types:
+
+#Number
+print(type(1))   # int - Integer - butun son
+print(type(1.5)) # float - Float - Haqiqiy son
+print(type(2 + 3j)) # complex
+
+
+print(type("Hello")) # str - string - So'z
+print(type("True")) # str - string - So'z
+
+print(type(True)) # bool
+print(type(False)) # bool
+
+# Reference/Container data type
+
+list
+tuple
+set
+dict
---
+++ third.py
@@ -0,0 +1,4 @@
+my_first_number = 5
+my_second_number = 10
+
+print(my_first_number + my_second_number)
